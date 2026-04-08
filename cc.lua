b328ddf1751eae6d3fcf454b1b44a8261bcbb26c
Gạo <nguyenanh0912clone@gmail.com>
2026-04-08 20:29:00 +0700

    Update cc.lua

--- cc.lua
+++ cc.lua
@@ -1,8 +1,8 @@
 _G.SeleneCFG = {
-    Team          = "Marines",   -- "Pirates" or "Marines"
+    Team          = "Pirates",   -- "Pirates" or "Marines"
     Region        = "Singapore",
     WebhookURL    = "https://discord.com/api/webhooks/1490967663457996832/R8qTO90kCfrdgVnLr2j8lfCAr6ZprGSVlc3h89_rdFIayvq-yd5iJ_HXSmJDhqYSyLHy",
     DiscordID     = "1276075309224955925",
-    SuperBoostFps     = false,
+    SuperBoostFps     = true,
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Idontknowbrodontstalk/selene/refs/heads/main/S%20ELENE-obfuscated.lua"))()
